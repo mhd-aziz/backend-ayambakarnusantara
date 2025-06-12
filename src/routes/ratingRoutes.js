e52d209194b06a35eaf232ddf6330bb717ff6521
mhd-aziz <muhamad17042002@gmail.com>
2025-06-12 22:09:42 +0700

    fix chat user fiture

--- src/routes/ratingRoutes.js
+++ src/routes/ratingRoutes.js
@@ -1,30 +1,25 @@
-// src/routes/ratingRoutes.js
 const express = require("express");
 const router = express.Router();
 const ratingController = require("../controllers/ratingController");
 const { authenticateToken } = require("../middlewares/authMiddleware");
 
-// Tambah rating baru ke produk
 router.post(
-  "/:productId", // productId dari produk yang dinilai
+  "/:productId",
   authenticateToken,
   ratingController.addRating
 );
 
-// Dapatkan semua rating untuk produk tertentu
 router.get(
-  "/:productId", // productId dari produk
+  "/:productId",
   ratingController.getRatingsForProduct
 );
 
-// Update rating yang sudah ada
 router.put(
-  "/:ratingId", // ratingId dari rating yang akan diupdate
+  "/:ratingId",
   authenticateToken,
   ratingController.updateRating
 );
 
-// Hapus rating yang sudah ada
 router.delete(
   "/:ratingId",
   authenticateToken,
